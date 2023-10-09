4c8a84bc9e7121d96306650f63b29059531a3959
Sergey <teprol112@bk.ru>
2023-10-09 16:49:54 +0400

    fix/поиск без валидации

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,23 +5,36 @@ import Button from "../Button/Button";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
 function SearchForm() {
-    return (
-        <section className="search-form">
-            <form action="#" method="get" className="search-form__form" name="get-movie">
-                <fieldset className="search-form__fieldset-search">
-                    <div className="search-form__container-search">
-                        <input type="search" className="search-form__search hover" placeholder="Фильм" name="key-word"></input>
-                        <Button className="search-form__submit button_search" type="submit">Поиск</Button>
-                    </div>
-                    <p className="search-form__errors"></p>
-                    <div className="search-form__container-checkbox">
-                        <FilterCheckbox></FilterCheckbox>
-                        <p className="search-form__checkbox-text">Короткометражки</p>
-                    </div>
-                </fieldset>
-            </form>
-        </section>
-    );
-};
+  return (
+    <section className="search-form">
+      <form
+        action="#"
+        method="get"
+        className="search-form__form"
+        name="get-movie"
+        noValidate
+      >
+        <fieldset className="search-form__fieldset-search">
+          <div className="search-form__container-search">
+            <input
+              type="search"
+              className="search-form__search hover"
+              placeholder="Фильм"
+              name="key-word"
+            ></input>
+            <Button className="search-form__submit button_search" type="submit">
+              Поиск
+            </Button>
+          </div>
+          <p className="search-form__errors"></p>
+          <div className="search-form__container-checkbox">
+            <FilterCheckbox></FilterCheckbox>
+            <p className="search-form__checkbox-text">Короткометражки</p>
+          </div>
+        </fieldset>
+      </form>
+    </section>
+  );
+}
 
 export default SearchForm;
